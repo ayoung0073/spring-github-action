e3223af2cce678f018ce5cdec06a0296f80f8b59
ayoung0073 <ayong0310@naver.com>
2021-08-13 20:30:59 +0900

    프로젝트 세팅

---
+++ src/test/java/com/may/springgithubaction/SpringGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may.springgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
