ad402ae0162541d949f873da8c97db543cd7b8a7
ayoung0073 <ayong0310@naver.com>
2021-08-19 11:22:15 +0900

    스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -31,4 +31,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다." >> /home/ubunt
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $JAR_NAME &
